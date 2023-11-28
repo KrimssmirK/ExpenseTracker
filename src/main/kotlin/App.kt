43588f0b55a2dd5a6dc01cf9ef1dae3bc137953f
KrimssmirK <krimssmirk003@gmail.com>
2023-11-28 15:20:47 +0800

    add balance, change title

--- src/main/kotlin/App.kt
+++ src/main/kotlin/App.kt
@@ -1,22 +1,64 @@
-fun main(args: Array<String>) {
-    // do something
-    var balance = 1000
+import java.time.LocalDate
+
+fun showMenu(currBalance: Balance) {
+    val date = LocalDate.now()
     println("--------------")
-    println("Monthly Budget")
+    println("${date.month} Budget")
     println("--------------")
-    println("Balance: P$balance")
+    println("Balance: P${currBalance.amount}")
     println("[1]Add Expenses")
     println("[2]Add Income")
+    println("[3]Add Balance")
+    println("[4]Exit")
     print("Enter: ")
-    var enter = readLine()
-    if (enter != null) {
-        when (enter.toInt()) {
-            1 -> println("enter 1")
-            2 -> println("enter 2")
-            else -> println("INVALID SELECTION")
-        }
-    } else {
-        println("INVALID")
-    }
+}
 
+fun addBalance(currBalance: Balance) {
+    var numeric = false
+    var balance: Int? = 0
+    while (!numeric) {
+        println("ADD BALANCE")
+        print("amount: ")
+        val balanceString = readLine()
+        try {
+            balance = balanceString?.toInt()
+            numeric = true
+        } catch (e: NumberFormatException) {
+            println("invalid format")
+        }
+    }
+    currBalance.amount = balance
+}
+
+fun selectMenu(currBalance: Balance) : Boolean {
+    var exit: Boolean = false
+    while (true) {
+        showMenu(currBalance)
+        var enter = readLine()
+        if (enter == "1") {
+            println("enter 1")
+            break
+        } else if (enter == "2"){
+            println("enter 2")
+            break
+        } else if (enter == "3") {
+            addBalance(currBalance)
+            break
+        } else if (enter == "4") {
+            exit = true
+            break
+        } else {
+            println("please enter 1 or 2")
+        }
+    }
+    return exit
+}
+
+fun main(args: Array<String>) {
+    val currBalance = Balance(0)
+    var exit = false
+    while (!exit) {
+        exit = selectMenu(currBalance)
+    }
+    println("Bye!")
 }
---
+++ src/main/kotlin/Balance.kt
@@ -0,0 +1,1 @@
+data class Balance(var amount: Int?)
